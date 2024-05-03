a468df7ce4ad3f95d3645a0b0df774c353618bc7
Matthew McKenzie <97194636+mattjamc@users.noreply.github.com>
2024-05-03 11:48:50 +0100

    Add asim log formatter

---
+++ asim_formatter.py
@@ -0,0 +1,102 @@
+import json
+import logging
+import os
+from datetime import datetime
+
+from flask import Request
+from flask import Response
+from flask import has_request_context
+from flask import request
+
+# from utils import get_package_version
+
+
+class ASIMFormatter(logging.Formatter):
+    def _get_event_result(self, response: Response) -> str:
+        event_result = "Success" if response.status_code < 400 else "Failure"
+
+        return event_result
+
+    def _get_file_name(self, request: Request) -> str:
+        if not request.files:
+            return "N/A"
+        if len(request.files.keys()) == 1:
+            return list(request.files.keys())[0]
+
+        return ";".join(list(request.files.keys())[0])
+
+    def _get_event_severity(self, log_level: str) -> str:
+        event_map = {
+            "DEBUG": "Informational",
+            "INFO": "Informational",
+            "WARNING": "Low",
+            "ERROR": "Medium",
+            "CRITICAL": "High",
+        }
+        return event_map[log_level]
+
+    def get_log_dict(self, record: logging.LogRecord) -> dict:
+        log_time = datetime.utcfromtimestamp(record.created).isoformat()
+
+        return {
+            "EventMessage": record.msg,
+            "EventCount": 1,
+            "EventStartTime": log_time,
+            "EventEndTime": log_time,
+            "EventType": "HTTPsession",
+            "EventSeverity": self._get_event_severity(record.levelname),
+            "EventOriginalSeverity": record.levelname,  # duplicate of above?
+            "EventSchema": "WebSession",
+            "EventSchemaVersion": "0.2.6",
+            # "IpFilterVersion": get_package_version(),
+        }
+
+        # TODO: look at expanding to include other fields from schema:
+        #  https://learn.microsoft.com/en-us/azure/sentinel/normalization-schema-web
+
+    def get_request_dict(self, request: Request) -> dict:
+        request_dict = {
+            "Url": request.url,
+            "UrlOriginal": request.url,
+            "HttpVersion": request.environ.get("SERVER_PROTOCOL"),
+            "HttpRequestMethod": request.method,
+            "HttpContentType": request.content_type,
+            "HttpContentFormat": request.mimetype,
+            "HttpReferrer": request.referrer,
+            "HttpUserAgent": str(request.user_agent),
+            "HttpRequestXff": request.headers.get("X-Forwarded-For"),
+            "HttpResponseTime": "N/A",
+            "HttpHost": request.host,
+            # TODO: add better support for multi-file upload and other file fields e.g. FileSize
+            "FileName": self._get_file_name(request),
+            "AdditionalFields": {
+                "TraceHeaders": {},
+            },
+        }
+
+        for trace_header in os.environ.get("DLFA_TRACE_HEADERS", ("X-Amzn-Trace-Id",)):
+            request_dict["AdditionalFields"]["TraceHeaders"][
+                trace_header
+            ] = request.headers.get(trace_header, None)
+
+        return request_dict
+
+    def get_response_dict(self, response: Response) -> dict:
+        return {
+            "EventResult": self._get_event_result(response),
+            "EventResultDetails": response.status_code,
+            "HttpStatusCode": response.status_code,
+        }
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_dict = self.get_log_dict(record)
+
+        if has_request_context():
+            request_dict = self.get_request_dict(request)
+            log_dict = log_dict | request_dict
+
+        if hasattr(record, "response"):
+            response_dict = self.get_response_dict(record.response)
+            log_dict = log_dict | response_dict
+
+        return json.dumps(log_dict)
