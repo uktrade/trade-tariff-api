9326eb1695153b9c55d44da24c1cf24351e9cb95
Emile Swarts <emile.swarts@digital.trade.gov.uk>
2024-05-31 11:31:44 +0100

    Debug downloading copilot

--- .copilot/phases/post_build.sh
+++ .copilot/phases/post_build.sh
@@ -3,5 +3,18 @@
 # Exit early if something goes wrong
 set -e
 
-df -h
+echo "We are downloading copilot"
+result=$(python <<EOF
+version="1.33.3"
+subprocess.run(
+    f"wget -q -O copilot-{version} https://ecs-cli-v2-release.s3.amazonaws.com/copilot-linux-v{version} && "
+    f"chmod +x ./copilot-{version} && mv copilot-{version} /copilot",
+    stdout=subprocess.PIPE,
+    stderr=subprocess.PIPE,
+    shell=True,
+    capture_output=True,
+)
+EOF
+
+echo $result
 # Add commands below to run as part of the post_build phase
